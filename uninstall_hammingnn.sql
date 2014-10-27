329470267484e642378c97eae3e4169f00f26764
Nicolas Pinchaud <nicolas.pinchaud@gmail.com>
2014-10-27 16:45:53 +0100

    Update uninstall_hammingnn.sql

--- uninstall_hammingnn.sql
+++ uninstall_hammingnn.sql
@@ -4,5 +4,6 @@
 SET search_path = public;
 
 DROP FUNCTION hammingnn (smallint, int4);
+DROP FUNCTION hammingball16 (smallint, int4);
 DROP FUNCTION hammingcheck16(bigint, bigint, bigint, bigint,smallint);
 DROP FUNCTION hammingcheck32(bigint, bigint, bigint, bigint,bigint, bigint, bigint, bigint, smallint);

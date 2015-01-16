dc75d51679f49ead9bc1a1b0a2e96f7ad14b39b2
Nicolas Pinchaud <nicolas.pinchaud@gmail.com>
2015-01-16 16:23:25 +0100

    Update uninstall_hammingnn.sql

--- uninstall_hammingnn.sql
+++ uninstall_hammingnn.sql
@@ -7,5 +7,6 @@ DROP FUNCTION hammingnn (smallint, int4);
 DROP FUNCTION hammingball16 (smallint, int4);
 DROP FUNCTION hammingball32 (int4, int4, int4);
 DROP FUNCTION hammingball64 (int8, int4);
+DROP FUNCTION hammingcheck8(bigint, bigint, smallint);
 DROP FUNCTION hammingcheck16(bigint, bigint, bigint, bigint,smallint);
 DROP FUNCTION hammingcheck32(bigint, bigint, bigint, bigint,bigint, bigint, bigint, bigint, smallint);
